ab9d3a5c38590cfd088a4d1e45a1222c97198ffc
ThibaudL <lamarchethibaud@hotmail.fr>
2016-09-01 21:03:59 +0200

    Init data

--- app/components/calendar/Calendar.js
+++ app/components/calendar/Calendar.js
@@ -3,13 +3,16 @@ angular.module('calendar.home', ['calendar.addevent'])
         templateUrl: 'app/components/calendar/calendar-template.html',
         controler : ['$log', '$state', 'EventService',function($log, $state, EventService, $ocLazyLoad) {
             var $ctrl = this;
+            
             $ctrl.$log = $log;
             $ctrl.$state = $state;
-            $ctrl.calendarView = 'month';
-            $ctrl.calendarDate = new Date();
-            $ctrl.calendarTitle = 'Elloguard events';
             
-            $ctrl.events = [];
+            $ctrl.$onInit = function(){
+                $ctrl.calendarView = 'month';
+                $ctrl.calendarDate = new Date();
+                $ctrl.calendarTitle = 'Elloguard events';
+                $ctrl.events = [];
+            }
             
             EventService.getAll().then(function(allEvents){
                 $ctrl.events = allEvents;
